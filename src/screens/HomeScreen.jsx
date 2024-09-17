319045a208b2075e14664590185fd39cabc17944
otzunun <olivertzunun.mingob@gmail.com>
2024-09-17 09:38:20 -0600

    showing logo

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,21 +1,28 @@
-// screens/HomeScreen.js
+
+
 import React from 'react';
 import styled from 'styled-components/native';
+import { Image, Text } from 'react-native';
+import logo from '../assets/logo.png';
 
 const Container = styled.View`
   flex: 1;
-  justify-content: center;
   align-items: center;
+  background: #ECFFE6;
 `;
 
 const Title = styled.Text`
-  font-size: 24px;
+  font-size: 20px;
   margin-bottom: 16px;
+  color: #262A26;
+  font-weight: bold;
+  
 `;
 
 const HomeScreen = () => (
-  <Container>
-    <Title>Home Screen</Title>
+  <Container >
+    <Image source={logo} style={{ width: 400, height: 400 }} />
+    <Title>Bienvenido a la aplicación</Title>
   </Container>
 );
 
