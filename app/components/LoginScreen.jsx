95608b76e458b62fd0136203701a8c0d3a25f91b
otzunun <olivertzunun.mingob@gmail.com>
2024-09-27 14:55:25 -0600

    new version

---
+++ app/components/LoginScreen.jsx
@@ -0,0 +1,94 @@
+
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default function LoginScreen() {
+  const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    if (username === 'olster' && password === 'daniela11') {
+      router.replace('/screens/Home');
+    }
+    else {
+      alert("Contraseña incorrecta");
+    }
+    // await AsyncStorage.setItem('userSession', 'true');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Usuario"
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Ingresar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.buttonSecondary}>
+        <Text style={styles.buttonText}>Olvidé mi contraseña</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#EFFFE0',
+    paddingTop: '50%'
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#000000',
+  },
+  input: {
+    width: '80%',
+    height: 50,
+    borderColor: '#d3d3d3',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    backgroundColor: '#FFFFFF',
+  },
+  button: {
+    width: '80%',
+    backgroundColor: '#4CAF50',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  buttonSecondary: {
+    width: '80%',
+    backgroundColor: '#6FCF97',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import LoginScreen from './components/LoginScreen';
+
+
+export default function Page() {
+  return (
+    <LoginScreen />
+  )
+}
