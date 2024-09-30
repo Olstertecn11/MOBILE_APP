e480709ddbb1dbcc55dab013bbc319f37628deb2
Olstertecn11 <otzunund@miumg.edu.gt>
2024-09-30 00:49:33 -0600

    last changes2

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,32 @@
-import { Text, View } from "react-native";
+
 import { useEffect } from "react";
 import { useRouter } from "expo-router";
 import LoginScreen from './components/LoginScreen';
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Page() {
-
   const router = useRouter();
 
   const checkSession = async () => {
-    const response = await AsyncStorage.getItem('user');
-    if (response !== null) {
-      router.replace('/Home');
+    try {
+      const user = await AsyncStorage.getItem('user');
+      console.log('Checking session...');
+
+      if (user !== null) {
+        console.log('Session found, redirecting to /Home');
+        router.replace('/Home'); // Redirige si hay una sesión activa
+      } else {
+        console.log('No session found, staying on login screen');
+      }
+    } catch (error) {
+      console.error('Error checking session:', error);
     }
-    console.log(response);
-  }
+  };
 
   useEffect(() => {
-    console.log('login');
     checkSession();
-
   }, []);
-  return (
-    <LoginScreen />
-  )
+
+  return <LoginScreen />;
 }
+
